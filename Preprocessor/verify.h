b15eae9dbb589c46f578d34517337297482778af
shubhamaher086 <shubhamaher777@gmail.com>
2022-12-14 17:51:55 +0530

    update header

--- Preprocessor/verify.h
+++ Preprocessor/verify.h
@@ -3,6 +3,6 @@
 #define verify(cond)\
     do{\
         if(!(cond))\
-            std::cerr << __FILE__ << ":" << __LINE__ << " In function main(), verification of condition \""<<#cond<<"\" failed.\n";\
+            std::cerr << __FILE__ << ":" << __LINE__ << " error:In function main(), verification of condition \""<<#cond<<"\" failed.\n";\
     } while (0)
 #endif
